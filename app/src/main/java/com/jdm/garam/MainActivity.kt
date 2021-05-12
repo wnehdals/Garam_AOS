ef01831b8efaf259fb38b9a70bf3d6cd6f24e1e9
Dongmin Joo <>
2021-05-12 14:00:47 +0900

    초기화

---
+++ app/src/main/java/com/jdm/garam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jdm.garam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
